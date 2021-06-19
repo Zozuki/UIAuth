052cc3c9be205676728f55b97620e33eadbd155c
Николай Унтевский <untewsky@yandex.ru>
2021-06-19 14:25:35 +0300

    Initial Commit

---
+++ TZUI/TZUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TZUIApp.swift
+//  TZUI
+//
+//  Created by user on 19.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct TZUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
